719c9f0bb7851019bcad4ca7a74af158b7fbcef9
Emmanuel Kourtakis <ekourtakis@gmail.com>
2025-12-17 00:26:32 -0500

    Add tests, delete core/tests.rs

--- src/core.rs
+++ src/core.rs
@@ -155,4 +155,15 @@ impl RushEngine {
 
 // --- TESTS ---
 #[cfg(test)]
-mod tests;
+mod tests {
+    use super::*;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_engine_initialization() {
+        let temp_dir = tempdir().unwrap();
+        let root = temp_dir.path().to_path_buf();
+        let _engine = RushEngine::with_root(root.clone()).unwrap();
+        assert!(root.join(".local/share/rush").exists());
+    }
+}
